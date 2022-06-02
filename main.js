95897ba626778290fab8fee50a1d465baf3eab00
NetanelYaso <netanelyaso12@gmail.com>
2022-06-02 15:45:33 +0300

    event-1

---
+++ main.js
@@ -0,0 +1,88 @@
+// function printAlert(){
+//     alert('hello');
+// }
+
+// function logName(){
+//     console.log('Netanel Yaso');
+// }
+
+// function changeSpanColor(){
+//     var color = document.getElementById('span_id');
+//     color.style.color ='red';
+// }
+
+// function changeH1Size(){
+//     var size = document.getElementById('h1_id');
+//     size.style.fontSize = '5vw';
+// }
+
+// function changeText(){
+//     var text = document.getElementById('h2_id');
+//     text.innerText = 'YOU A BITCH ASS NIGGA';
+// }
+
+// function printName(){
+//     var user = prompt('enter name');
+//     var myButton = document.getElementById('button1');
+//     myButton.innerHTML = user ;
+// }
+
+// window.onload = function dateTime(){
+//     var time = new Date()
+//     if(time.getHours()<12){
+//         document.body.style = 'background : yellow';
+//        var x = document.getElementById('my_H1');
+//        x.innerHTML = 'good day';
+//     }
+//     else{
+//         document.body.style = 'background : blue';
+//         var x = document.getElementById('my_H1');
+//         x.innerHTML = 'good envning';
+//     }
+// }
+
+
+// window.onload = function age18(){
+//     var user = +prompt('enter age');
+//     var age1 = document.getElementById("myh1")
+//     if(user<18){
+//         age1.style.color = "red";
+//        age1.innerHTML = "welcome";
+
+//     }
+//     else{
+//         age1.style.color = "blue";
+//         age1.innerHTML = "welcome";
+//     }    
+// }
+
+// function changeToGreen(){
+//     var x = document.getElementById('myh1');
+//     x.style.color= 'green';
+// }
+
+// var myArray = [];
+// function mainFunc(){
+//     if(numbersArray.length<5){
+//         nums.user.innerText += user_number.value + " ";
+//         numbersArray.push(user_number.value);
+//     }
+//     else{
+//         user_number.disabled = true;
+//     }
+// }
+
+// function getAndPrintRandom(){
+//     var rndNum = Math.floor(Math.random()*57);
+//     rnd_h3.innerText += rndNum;
+//     for(var i=0; i< numberArray.length; i++){
+//         if(rndNum==numbersArray[i]){
+//             result_text.innerText ="You win!";
+//             return;
+//         }
+//     }
+//     result_text.innerText = "You Loose!";
+// }
+
+
+
